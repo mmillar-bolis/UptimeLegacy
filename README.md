dccde2bc8780b5580464917c8c75cbe3b54470b3
M.Millar <mmillar@bolis.com>
2020-03-24 21:51:34 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# UptimeLegacy
-Provides system uptime information for Windows PowerShell 5 and below.
+# Uptime (Legacy)
+
+A module that provides system uptime information for Windows PowerShell 5 and below.
+
+Available on [PSGallery](https://www.powershellgallery.com/packages/Uptime). Try it out with:
+```powershell
+Install-Module -Name Uptime -Repository PSGallery -Scope CurrentUser
+```
+
+## Description
+
+This is a functional duplicate to the Get-Uptime cmdlet available in PowerShell 6 and above. For Windows NT specifically, I tap into the same set of objects via WMI and CIM.
+
+### Example 1
+
+```powershell
+Get-Uptime
+
+Days              : 9
+Hours             : 0
+Minutes           : 9
+Seconds           : 45
+Milliseconds      : 0
+Ticks             : 7781850000000
+TotalDays         : 9.00677083333333
+TotalHours        : 216.1625
+TotalMinutes      : 12969.75
+TotalSeconds      : 778185
+TotalMilliseconds : 778185000
+```
+
+This will display an object whose members comprise the system uptime.
+
+### Example 2
+
+```powershell
+Get-Uptime -Since
+
+Tuesday, June 18, 2019 2:34:56 PM
+```
+
+This will reate a symbolic link to test.exe in the $env:USERPROFILE\bin folder. Note that $env:USERPROFILE is evaluated in the context of the caller process.
+
+### Thanks for looking!
+
+Have a look at the help documentation at [docs.microsoft.com](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-uptime?view=powershell-7).
---
+++ Uptime.psm1
@@ -0,0 +1,65 @@
+# Microsoft Windows Powershell Script
+#
+# Name: Uptime
+# Version: 0.1.1.0
+# Date: 2020-03-24
+# Author: MMillar
+# https://github.com/mmillar-bolis
+# http://www.bolis.com
+#
+# License:
+# The MIT License
+# Copyright (c) 2020, The Bolis Group
+#
+# Description:
+# Get system uptime for PowerShell releases 2 through 5.
+#
+# This is a functional clone of the Get-Uptime cmdlet that is available in
+# PowerShell versions 6 and later. It has been tested in versions 2, and 5.1 to
+# reliably provide the same output as the official cmdlet.
+#
+# I have added a warning for shells after version 5 to notify the user of the
+# legacy status of this module, in case it clobbers over the built-in.
+#
+
+#
+function Get-Uptime {
+	[CmdletBinding(
+		SupportsShouldProcess=$true
+	)]
+	param(
+		[switch]$Since
+	)
+	begin {
+		$Shell_Version = $PSVersionTable.PSversion.Major
+		$Uptime = @()
+		if ( $Shell_Version -gt 5 ) {
+			Write-Warning "This is a legacy module intended for PowerShell releases prior to version 6." -WarningAction Continue
+		}
+	}
+	process {
+		if ( $Shell_Version -lt 4 ) {
+			$Data_Query = Get-WmiObject -Class Win32_OperatingSystem
+			$Data_Table = $Data_Query.ConvertToDateTime($Data_Query.LocalDateTime) - $Data_Query.ConvertToDateTime($Data_Query.LastBootUpTime)
+		} else {
+			$Data_Query = Get-CimInstance -ClassName Win32_OperatingSystem
+			$Data_Table = $Data_Query.LocalDateTime - $Data_Query.LastBootUpTime
+		}
+
+		$Uptime = $Data_Table.PSObject.Copy()
+
+	}
+	end {
+        if ($Since) {
+			if ( $Shell_Version -lt 4 ) {
+				$Data_Query.ConvertToDateTime($Data_Query.LastBootUpTime)
+			} else {
+				$Data_Query.LastBootUpTime
+			}
+		} else {
+			$Uptime
+		}
+	}
+}
+
+New-Alias -Name 'uptime' -Value 'Get-Uptime'
